d850e3f1a8b8ca274aab76f68b1e71af0a2af58e
solomon344 <willamssolomon672@gmail.com>
2026-05-17 21:28:10 +0000

    fix: correct API_URL format and enable debug mode in Aptabase initialization

--- aptabase-init.js
+++ aptabase-init.js
@@ -1,10 +1,12 @@
 import { init, trackEvent } from './node_modules/@aptabase/web/dist/index.js';
 
 const APP_KEY = 'A-SH-1180694718';
-const API_URL = 'https://analytics.vps.divparser.com';
+const API_URL = 'https://analytics.vps.divparser.com/';
 
 init(APP_KEY, {
+  // host: API_URL,
   host: API_URL,
+  isDebug: false
   // If you want to use `host` instead, replace this with:
   // host: 'https://analytics.vps.divparser.com',
 });
